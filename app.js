062b61b585863c3c2853e1cb7c0bae34d48357f9
Kyle Koopman <kylekoopman@gmail.com>
2013-08-22 21:47:17 -0700

    Created the app file, and included all express routes

---
+++ app.js
@@ -0,0 +1,60 @@
+/**
+ * Module dependencies.
+ */
+
+ 'use strict';
+
+var express = require('express'),
+    routes = require('./routes'),
+    fs = require('fs');
+
+
+
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+app.get('/api/trade', function(req,res){
+console.log("test");
+});
+
+app.get('/form', function(req, res) {
+	fs.readFile('./form.html', function(error, content) {
+		if (error) {
+			res.writeHead(500);
+			res.end();
+		} else {
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(content, 'utf-8');
+		}
+	});
+});
+
+app.post('/signup', function(req, res) {
+  console.log("test");
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
